8ce61ccd8fd50e458db3c140b5551a07f0db05bc
Dan <danielpowell12@msn.com>
2018-07-08 23:29:11 -0600

    Builds out dummy dashboard
    Overtime this will become nice.
    
    This is a quick demo that fetches the users gardens from the api
    and loops over them with show cards and links.
    
    If no gardens exist, the view pushes a (new) user to create a garden

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,13 +9,51 @@ const LoggedOutHome = _ => (
     <p>Todo: insert marketing page.</p>
   </section>
 );
-const LoggedInHome = _ => (
-  <section>
-    <h1>Home</h1>
-    <h3>You are logged in.</h3>
-    <p>Todo: insert a dashboard.</p>
-  </section>
-);
+
+class LoggedInHome extends Component {
+  state = {
+    gardens: []
+  };
+
+  componentDidMount() {
+    this.loadGardens();
+  }
+
+  loadGardens = () => {
+    return GET("/gardens")
+      .then(({ gardens }) => this.setState({ gardens }))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    const { gardens } = this.state;
+    return (
+      <section>
+        <h1>Home</h1>
+        <h3>You are logged in.</h3>
+        <p>Todo: insert a dashboard.</p>
+        <div>
+          <h2>My Gardens</h2>
+          <div>
+            {gardens.length ? (
+              gardens.map(({ id, name, population }, i) => (
+                <div key={i}>
+                  <p>Name: {name}</p>
+                  <p>Population: {population}</p>
+                  <Link to={`/gardens/${id}`}>Manage Plot</Link>
+                </div>
+              ))
+            ) : (
+              <p>
+                No seeds started. <Link to="/gardens/new">Start A Garden</Link>
+              </p>
+            )}
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 
 const Home = _ =>
   Auth.isAuthenticated() ? <LoggedInHome /> : <LoggedOutHome />;
